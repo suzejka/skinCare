b72a72a2bbde00e26bec0f8fa2077aa73e45d7e5
Zuzanna Stasiak <zuzanna.julia.stasiak@gmail.com>
2022-08-28 18:47:55 +0200

    fix

--- silnik.py
+++ silnik.py
@@ -137,7 +137,7 @@ def showGUI(dum_df, dataset, products):
     st.set_page_config(
      page_title="System rekomendacyjny, do tworzenia planów pielęgnacyjnych",
      menu_items={
-        'Report a bug': "https://forms.gle/5KV7rdhNi8epigL26"
+        'Report a bug': "https://forms.gle/5KV7rdhNi8epigL26",
          'About': "# Praca inżynierska. *s20943*"
         }
     )
